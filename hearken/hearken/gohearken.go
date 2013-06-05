ef970c46707fe02636eff35ecb0eef62ddca40ff
zxc@hearken.me <zxc@hearken.me>
2013-06-05 15:18:22 +0800

    change name

---
+++ hearken/hearken/gohearken.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "flag"
+    "os"
+    "goHearken/hearken"
+)
+
+func main(){
+    flag.Parse()
+    args :=flag.Args()
+    if len(args)==0 ||len(args)>3 {
+    os.Exit(1)
+    }
+    switch args[0] {
+        case "init":
+            hearken.ProjectInit(args[1],args[2])
+        case "post":
+            postInfo:=hearken.ReadConfig()
+            hearken.GeneratePost(postInfo.WebRoot,postInfo.PerPage)
+        }
+
+}
